820de5c84a82d11ba1d8079b3caed317fc655375
shen <bear_shen@live.cn>
2026-06-30 22:22:18 +0800

    compose f

--- compose/start_service.sh
+++ compose/start_service.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+chown -R www-data:www-data /var/www/html
+chown -R postgres:postgres /var/lib/postgresql/16/main
+
 echo "[init] starting postgresql..."
 service postgresql start
 
