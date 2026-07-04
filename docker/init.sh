ddd82cb2e9422fcfa301e2b1ef111b355a11d715
shen <bear_shen@live.cn>
2026-07-04 17:31:46 +0800

    php conf compose

--- docker/init.sh
+++ docker/init.sh
@@ -12,7 +12,7 @@ chmod +x ${SRC}/nvm_install.sh
 nvm install 24
 
 apt install -y nginx-full
-apt install -y php-fpm php-cli php-pgsql php-gd php-curl php-mbstring php-json php-xml php-zip php-date php-readline php-bz2 
+apt install -y php-fpm php-cli php-pgsql php-gd php-curl php-mbstring php-json php-xml php-zip php-date php-readline php-bz2 php-mysql 
 
 apt install -y ca-certificates lsb-release
 wget https://packages.groonga.org/ubuntu/groonga-apt-source-latest-$(lsb_release --codename --short).deb
